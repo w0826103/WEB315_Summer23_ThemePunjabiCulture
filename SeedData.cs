894e7ea319f7f438dd6280028223e42d94e24cfd
w0826103 <124601041+w0826103@users.noreply.github.com>
2023-02-09 19:03:43 -0500

    seed data updated

--- SeedData.cs
+++ SeedData.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -6,7 +5,7 @@ using System.Linq;
 
 namespace WEB315_Summer23_ThemePunjabiCulture.Models
 {
-    public static class SeedData
+    public static class SeedData_PunjabiCulture
     {
         public static void Initialize(IServiceProvider serviceProvider)
         {
@@ -14,71 +13,63 @@ namespace WEB315_Summer23_ThemePunjabiCulture.Models
                 serviceProvider.GetRequiredService<
                     DbContextOptions<PunjabiCultureContext>>()))
             {
-                // Look for any movies.
+                .
                 if (context.PunjabiCulture.Any())
                 {
                     return;   // DB has been seeded
                 }
-
-                context.PunjabiCulture.AddRange(
-                    new PunjabiCulture
-                    {
-                        CultureName = "Diwali",
+                else
+                {
+                    context.PunjabiCulture.AddRange(
+                        new PunjabiCulture
+                        {
+                            CultureName = "Diwali",
                         OccassionDate = DateTime.Parse("2023-11-12"),
                         Costume = "jeans",
                         Food = "dal roti",
-                        OrganisedBy = "gurbinder",
-                        
-                        
-                    },
+                        OrganisedBy = "gurbinder"
+                        },
 
-                    new PunjabiCulture
-                    {
-                        CultureName = "Holi",
+                        new PunjabiCulture
+                        {
+                            CultureName = "Holi",
                         OccassionDate = DateTime.Parse("2023-04-12"),
                         Costume = "kurta pajama",
                         Food = "saag",
-                        OrganisedBy = "manpreet",
-                        
-                        
-                    },
+                        OrganisedBy = "manpreet"
+                        },
 
-                    new PunjabiCulture
-                    {
-                        CultureName = "new year",
+                        new PunjabiCulture
+                        {
+                            CultureName = "new year",
                         OccassionDate = DateTime.Parse("2023-01-01"),
                         Costume = " suit",
                         Food = " dhee",
-                        OrganisedBy = "karam",
-                        
-                        
-                    },
+                        OrganisedBy = "karam"
+                        },
 
-                    new PunjabiCulture
-                    {
-                        CultureName = "dusshera",
+                        new PunjabiCulture
+                        {
+                            CultureName = "dusshera",
                         OccassionDate = DateTime.Parse("2023-10-12"),
                         Costume = "tehmat",
                         Food = "paneer",
-                        OrganisedBy = "harry",
-                        
-                        
-                    },
+                        OrganisedBy = "harry"
+                        },
 
-                    new PunjabiCulture
-                    {
-                        CultureName = "vaisakhi",
+                        new PunjabiCulture
+                        {
+                            CultureName = "vaisakhi",
                         OccassionDate = DateTime.Parse("2023-04-14"),
                         Costume = "phulkari",
                         Food = "mix veg",
-                        OrganisedBy = "meenu",
-                        
-                        
-                    },
-                   
-                );
-                context.SaveChanges();
+                        OrganisedBy = "meenu"
+                        }
+                    );
+                    context.SaveChanges();
+                }
+                
             }
-        }
-    }
+        }
+    }
 }
